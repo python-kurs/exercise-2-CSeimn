3da3e38f363b2992906da0db3ffe4059d4667bce
CSeimn <44396406+CSeimn@users.noreply.github.com>
2019-05-13 12:04:59 +0200

    Update second_steps.py

--- second_steps.py
+++ second_steps.py
@@ -10,12 +10,24 @@ sat_database = {"METEOSAT" : 3000,
 
 # 1) Add the "GOES" and "worldview" satellites with their 2000/0.31m resolution to the dictionary [2P]
 
-print("I have the following satellites in my database:")
+sat_database = {"METEOSAT" : 3000,
+                "LANDSAT"  : 30,
+                "MODIS"    : 500,
+                "GOES"     : 2000,
+                "worldview": 0.31
+               }
+
+print("I have the following satellites in my database:", sat_database.keys())
 
 # 2) print out all satellite names contained in the dictionary [2P]
-
+print(sat_database.keys())
 # 3) Ask the user to enter the satellite name from which she/he would like to know the resolution [2P]
-
+search = input("Which sattelite are you searching ? ")
 # 4) Check, if the satellite is in the database and inform the user, if it is not [2P]
-
+answer = search in sat_database
+print(answer)
 # 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P] 
+if answer == True:
+    print("Your sattelite exists in this database!")
+elif answer == False:
+    print("Your sattelite is not found in this database!")
