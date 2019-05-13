9d885d86c455f6fc5d635ea1361dc1f12d526263
CSeimn <44396406+CSeimn@users.noreply.github.com>
2019-05-13 12:25:16 +0200

    Update second_steps.py

--- second_steps.py
+++ second_steps.py
@@ -25,9 +25,20 @@ print(sat_database.keys())
 search = input("Which sattelite are you searching ? ")
 # 4) Check, if the satellite is in the database and inform the user, if it is not [2P]
 answer = search in sat_database
-print(answer)
-# 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P] 
 if answer == True:
-    print("Your sattelite exists in this database!")
+    print("Your sattelite exists in this database! ")
 elif answer == False:
-    print("Your sattelite is not found in this database!")
+    print("Your sattelite is not found in this database! ")
+# 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P] 
+if search == "METEOSAT":
+    print("The METEOSAT-sattelite has a resolution of 3000.")
+elif search == "LANDSAT":
+    print("The LANDSAT-sattelite has a resolution of 30.")
+elif search == "MODIS":
+    print("The MODIS-sattelite has a resolution of 500.")
+elif search == "GOES":
+    print("The GOES-sattelite has a resolution of 2000.")
+elif search == "worldview":
+    print("The worldview-sattelite has a resolution of 0.31.")
+else:
+    print("Try a new search.")
